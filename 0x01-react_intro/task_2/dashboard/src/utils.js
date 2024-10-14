18e9d34344c9b46b8720546cb1fd81b08ad197d3
youssefelghamour <youssefelghamour@gmail.com>
2024-10-14 18:26:59 +0100

    4. Modify the Notifications: task_2

--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -5,3 +5,9 @@ export function getFullYear() {
 export const getFooterCopy = (isIndex) => {
     return isIndex ? "Holberton School" : "Holberton School main dashboard";
 }
+
+export function getLatestNotification() {
+    return (
+        '<strong>Urgent requirement</strong> - complete by EOD'
+    );
+}
