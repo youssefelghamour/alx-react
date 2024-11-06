4895b8c41be29dd983b28b8ae5faec9715c46352
youssefelghamour <youssefelghamour@gmail.com>
2024-11-06 18:28:28 +0100

    5. Selectors

---
+++ 0x08-react_redux_reducer_selector/task_5/dashboard/src/actions/uiActionCreators.js
@@ -0,0 +1,51 @@
+import { DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER, LOGIN, LOGIN_FAILURE, LOGIN_SUCCESS, LOGOUT } from "./uiActionTypes";
+
+
+export const login = (email, password) => ({
+    type: LOGIN,
+    user : { email, password },
+});
+
+export const boundLogin = (email, password) => dispatch(login(email, password));
+
+
+export const logout = () => ({
+    type: LOGOUT,
+});
+
+export const boundLogout = () => dispatch(logout());
+
+
+export const displayNotificationDrawer = () => ({
+    type: DISPLAY_NOTIFICATION_DRAWER,
+});
+
+export const boundDisplayNotificationDrawer = () => dispatch(displayNotificationDrawer());
+
+
+export const hideNotificationDrawer = () => ({
+    type: HIDE_NOTIFICATION_DRAWER,
+});
+
+export const boundHideNotificationDrawer = () => dispatch(hideNotificationDrawer());
+
+
+export const loginSuccess = () => ({
+    type: LOGIN_SUCCESS,
+});
+
+
+export const loginFailure = () => ({
+    type: LOGIN_FAILURE,
+});
+
+
+export const loginRequest = (email, password) => {
+    return (dispatch) => {
+        dispatch(login(email, password));
+
+        return fetch('http://localhost:8080/login-success.json')
+            .then(() => dispatch(loginSuccess()))
+            .catch((error) => dispatch(loginFailure()));
+    };
+};
---
+++ 0x08-react_redux_reducer_selector/task_5/dashboard/src/reducers/uiReducer.test.js
@@ -0,0 +1,20 @@
+import { DISPLAY_NOTIFICATION_DRAWER } from "../actions/uiActionTypes";
+import { initialState, uiReducer } from "./uiReducer";
+
+
+describe('uiReducer', () => {
+    it('returns the initial state when no action is passed', () => {
+        expect(uiReducer(undefined, {})).toEqual(initialState);
+    });
+
+    it('returns the initial state when the action SELECT_COURSE is passed', () => {
+        const action = { type: 'SELECT_COURSE' };
+        expect(uiReducer(undefined, action)).toEqual(initialState);
+    });
+
+    it('returns the state with updated isNotificationDrawerVisible property when the action DISPLAY_NOTIFICATION_DRAWER is passed', () => {
+        const action = { type: DISPLAY_NOTIFICATION_DRAWER };
+        const result = initialState.set('isNotificationDrawerVisible', true);
+        expect(uiReducer(undefined, action)).toEqual(result);
+    });
+});
